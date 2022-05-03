b8f61d9257e75713526b968ec22cd373f884316b
porntittaya.udm@gmail.com <amontep.wij@kdlab.ai>
2022-05-03 12:02:24 +0700

    add load image

--- face_detection/detector.py
+++ face_detection/detector.py
@@ -16,6 +16,28 @@ def relative(path):
     return os.path.abspath(path)
 
 
+def get_image(img_path):
+    if type(img_path) == str:  # Load from file path
+        if not os.path.isfile(img_path):
+            raise ValueError(
+                "Input image file path (", img_path, ") does not exist.")
+        img = cv2.imread(img_path)
+
+    elif isinstance(img_path, np.ndarray):  # Use given NumPy array
+        img = img_path.copy()
+
+    else:
+        raise ValueError(
+            "Invalid image input. Only file paths or a NumPy array accepted.")
+
+    # Validate image shape
+    if len(img.shape) != 3 or np.prod(img.shape) == 0:
+        raise ValueError(
+            "Input image needs to have 3 channels at must not be empty.")
+
+    return img
+
+
 class RetinaFace:
     def __init__(
         self,
@@ -25,11 +47,15 @@ class RetinaFace:
     ):
         self.gpu_id = gpu_id
         self.device = (
-            torch.device("cpu") if gpu_id == -1 else torch.device("cuda", gpu_id)
+            torch.device("cpu") if gpu_id == -
+            1 else torch.device("cuda", gpu_id)
         )
         self.model = load_net(model_path, self.device, network)
 
-    def detect(self, images):
+    def detect(self, img_path):
+
+        images = get_image(img_path)
+
         if isinstance(images, np.ndarray):
             if len(images.shape) == 3:
                 return batch_detect(self.model, [images], self.device)[0]
